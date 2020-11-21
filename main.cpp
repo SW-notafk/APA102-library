b2fed8c55e28dd4c72de3c577e2915b63b98d502
SW-notafk <57134347+SW-notafk@users.noreply.github.com>
2020-11-21 16:46:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <avr/io.h>
+///Including the header file 
 #include "calling.h"
+///Specifying the clock speed
 #define F_CPU 16000000
 
-
+///Main function for the program
 int main(void)
 {
 	LedStrip ledstrip;
 	ledstrip.LedInit();
 	
-    while (1) 
-    {
-		ledstrip.DisplayText("");
-    }
+    while (1)
+	{
+		
+	} 
+    
 }
-
